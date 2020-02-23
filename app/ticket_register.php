e019ec8cca607c9021eb4984b8665c1eb31e5a0f
SoulIsMyDiet <marekd.90@gmail.com>
2020-02-24 00:27:07 +0100

    some clean up

--- app/ticket_register.php
+++ app/ticket_register.php
@@ -16,19 +16,14 @@ $startDate = DateTime::createFromFormat('Y-m-d H:i:s', $date.' '.$time)->format(
 $dateTime = New DateTime($startDate, new DateTimeZone('Europe/Warsaw'));
 $endDate = $dateTime->add(new DateInterval("PT30M"));
 $endDate = $endDate->format('Y-m-d H:i:s');
-//var_dump($_POST);exit;
 $dsn = "mysql:host=".DBHOST.";dbname=".DBNAME.";port=3306";
 $pdo = new PDO($dsn, DBUSER, DBPWD);
-//var_dump($name, $userId, $description, $startDate, $endDate);exit;
-//$startDate = '1990-03-02 02:02:00';
-//$endDate = '1990-03-02 02:32:00';
-try {
-//    $pdo->exec("INSERT INTO tickets (plates, user_id, description) VALUES ('$plates', '$userId', '$description');");
-    $pdo->exec("INSERT INTO tickets (plates, user_id, description , start, end) VALUES ('$plates', $userId, '$description', '$startDate', '$endDate');");
-        var_dump($pdo->errorInfo());exit;
-
-}catch(Exception $e){
-    echo 'Exception -> ';
-    var_dump($e->getMessage());
-}
+    $res = $pdo->exec("INSERT INTO tickets (plates, user_id, description , start, end) VALUES ('$plates', $userId, '$description', '$startDate', '$endDate');");
 $pdo = null;
+if($res){
+    $_SESSION['register_ticket'] = 'Udało się zarejestrować zgłoszenie!';
+}else{
+    $_SESSION['register_ticket'] = 'Nie udało się zarejestrować zgłoszeniea:) Spróbuj niedługo ponownie';
+}
+header("Location: ../panel.html");
+exit();
