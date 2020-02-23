729896d1de56ad319fbe747243c45ba42608b9b1
SoulIsMyDiet <marekd.90@gmail.com>
2020-02-23 13:20:44 +0100

    Working register/save to database

--- app/users.php
+++ app/users.php
@@ -1,8 +1,6 @@
 <?php
 include_once  __DIR__.'/../config/config.php';
-require_once __DIR__ . '/../vendor/autoload.php';
 
-//var_dump(__DIR__.'/../config/config.php);exit;
 $name = $_POST['name'];
 $surname = $_POST['surname'];
 $pesel = $_POST['pesel'];
@@ -12,23 +10,5 @@ $phone = $_POST['phone'];
 $password = $_POST['password'];
 $description = $_POST['description'];
 $dsn = "mysql:host=".DBHOST.";dbname=".DBNAME.";port=3306";
-//var_dump($dsn);exit;
-//try {
 $dbh = new PDO($dsn, DBUSER, DBPWD);
-//    $conn = mysqli_connect($dsn, DBUSER, DBPWD);
-//var_dump($conn);exit;
-//    if (!$conn) {
-//        die("Connection failed: " . mysqli_connect_error());
-//    }
-//    echo "Connected successfully";
-//var_dump($dbh);exit;
-//var_dump($dsn, DBUSER, DBPWD);exit;
-//$dbh->prepare("INSERT INTO users (name, surname, pesel, nip, email, phone, password, description) VALUES (?,?,?,?,?,?,?,?);")
-//    var_dump($dbh);exit;
-$dbh->exec( "INSERT INTO users (name, surname, pesel, nip, email, phone, password, description) VALUES ($name, $surname, $pesel, $nip, $email, $phone, $password, $description);" );
-//} catch (PDOException $e) {
-//    echo "Failed to get DB handle: " . $e->getMessage() . "\n";
-//    exit;
-//}
-//var_dump($dbh->exec( "INSERT INTO users (name, surname, pesel, nip, email, phone, password, description) VALUES ($name, $surname, $pesel, $nip, $email, $phone, $password, $description);" ));exit;
-//var_dump($lol);exit;
+$dbh->exec( "INSERT INTO users (name, surname, pesel, nip, email, phone, password, description) VALUES ('$name', '$surname', '$pesel', '$nip', '$email', '$phone', '$password', '$description');" );
